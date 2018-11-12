cb93a9fd92244a5d52d4f92c90e36a9d43bf8e96
joseph-rato <joseph.rato@gmail.com>
2018-11-12 10:57:52 -0800

    fixed login bug

--- frontend/components/session_forms/session_form.jsx
+++ frontend/components/session_forms/session_form.jsx
@@ -18,6 +18,7 @@ class SessionForm extends React.Component {
 
 
   handleSubmit(e) {
+    e.preventDefault()
     const user = Object.assign({}, this.state)
     this.props.processForm(user).then(this.props.closeModal)
   }
