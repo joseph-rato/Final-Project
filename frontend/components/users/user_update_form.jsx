a703c9a93cf57a14b068ee4c22752c7180e46a67
joseph-rato <joseph.rato@gmail.com>
2018-11-15 13:49:04 -0800

    update profile has user information through async

--- frontend/components/users/user_update_form.jsx
+++ frontend/components/users/user_update_form.jsx
@@ -6,20 +6,23 @@ class UserUpdateForm extends React.Component{
         super(props)
         debugger
         this.state = {
-            username: this.props.users[this.props.userId] ? this.props.users[this.props.userId].username : "",
-            avatar: this.props.users[this.props.userId] ? this.props.users[this.props.userId].avatar : "",
-            headline: this.props.users[this.props.userId] ? this.props.users[this.props.userId].headline : "",
-            website: this.props.users[this.props.userId] ? this.props.users[this.props.userId].website : "",
-            email: this.props.users[this.props.userId] ? this.props.users[this.props.userId].email : "",
-            profile_header: this.props.users[this.props.userId] ? this.props.users[this.props.userId].profile_header : ""
+            user: ""
+            // username: this.props.users[this.props.userId] ? this.props.users[this.props.userId].username : "",
+            // avatar: this.props.users[this.props.userId] ? this.props.users[this.props.userId].avatar : "",
+            // headline: this.props.users[this.props.userId] ? this.props.users[this.props.userId].headline : "",
+            // website: this.props.users[this.props.userId] ? this.props.users[this.props.userId].website : "",
+            // email: this.props.users[this.props.userId] ? this.props.users[this.props.userId].email : "",
+            // profile_header: this.props.users[this.props.userId] ? this.props.users[this.props.userId].profile_header : ""
         }
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleChange = this.handleChange.bind(this)
         this.handleFile = this.handleFile.bind(this)
     }
     componentDidMount(){
-        this.props.fetchUser(this.props.userId);
-        debugger 
+        debugger
+        this.props.fetchUser(this.props.userId).then( (userData) => 
+            this.setState(userData)
+        )
     }
 
     handleFile(type){
@@ -44,41 +47,46 @@ class UserUpdateForm extends React.Component{
 
     render(){
         debugger
-        return(
-            <div className="user-form-page">
-                <h1 className="page-description">Settings</h1>
-                <div className="update-form-container">
-                    <h3>My Details</h3>
-                    <form onSubmit={this.handleSubmit}>
-                        <div className="profile-form-name">
-                            <label className="profile-update-label">Name</label>
-                            <input type="text" className="profile-form-input-field" onChange={this.handleChange('username')} value={this.state.username}></input>
-                        </div>
-                        <div className="profile-form-avatar">
-                            <label className="profile-update-label">Avatar</label>
-                            <input type="file"></input>
-                        </div>
-                        <div className="profile-form-headline">
-                            <label className="profile-update-label">Headline</label>
-                            <input type="text" className="profile-form-input-field" onChange={this.handleChange('headline')} value={this.state.headline}></input>
-                        </div>
-                        <div>
-                            <label className="profile-update-label">Email</label>
-                            <input type="email" className="profile-form-input-field" onChange={this.handleChange('email')} value={this.state.email}></input>
-                        </div>
-                        <div>
-                            <label className="profile-update-label">Website</label>
-                            <input type="text" className="profile-form-input-field" onChange={this.handleChange('website')} value={this.state.website}></input>
-                        </div>
-                        <div>
-                            <label className="profile-update-label">Profile Header</label>
-                            <input type="file"></input>
-                        </div>
-                        <button type="submit" className="">UPDATE</button>
-                    </form>
+        this.user = this.state.user
+        if (this.user === ""){
+            return null;
+        } else {
+            return(
+                <div className="user-form-page">
+                    <h1 className="page-description">Settings</h1>
+                    <div className="update-form-container">
+                        <h3>My Details</h3>
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="profile-form-name">
+                                <label className="profile-update-label">Name</label>
+                                <input type="text" className="profile-form-input-field" onChange={this.handleChange('username')} value={this.user.username}></input>
+                            </div>
+                            <div className="profile-form-avatar">
+                                <label className="profile-update-label">Avatar</label>
+                                <input type="file"></input>
+                            </div>
+                            <div className="profile-form-headline">
+                                <label className="profile-update-label">Headline</label>
+                                <input type="text" className="profile-form-input-field" onChange={this.handleChange('headline')} value={this.user.headline}></input>
+                            </div>
+                            <div>
+                                <label className="profile-update-label">Email</label>
+                                <input type="email" className="profile-form-input-field" onChange={this.handleChange('email')} value={this.user.email}></input>
+                            </div>
+                            <div>
+                                <label className="profile-update-label">Website</label>
+                                <input type="text" className="profile-form-input-field" onChange={this.handleChange('website')} value={this.user.website}></input>
+                            </div>
+                            <div>
+                                <label className="profile-update-label">Profile Header</label>
+                                <input type="file"></input>
+                            </div>
+                            <button type="submit" className="">UPDATE</button>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        }
     }
 }
 
